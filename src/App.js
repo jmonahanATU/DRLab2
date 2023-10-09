b519df28ee66bea207fb321f3945f26e8aacbde3
Josh <g00409871@atu.ie>
2023-10-09 18:56:27 +0100

    lab2compl

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-    import './App.css';
-    import Header from './components/header';
-    import Content from './components/content';
-    import Footer from './components/footer';
-    import 'bootstrap/dist/css/bootstrap.min.css';
-    import Container from 'react-bootstrap/Container';
-    import Nav from 'react-bootstrap/Nav';
-    import Navbar from 'react-bootstrap/Navbar';
-    import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+// Importing CSS styles from App.css
+import './App.css';
 
+// Importing components from their respective files
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
 
+// Importing Bootstrap CSS and specific components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+// Importing necessary routing components from react-router-dom
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
+// Define the main App functional component
 function App() {
   return (
+    // Using BrowserRouter to handle routing
     <BrowserRouter>
       <div className="App">
-          <Navbar bg="primary" data-bs-theme="dark">
-            <Container>
-              <Navbar.Brand href="/">Navbar</Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/read">Read</Nav.Link>
-                <Nav.Link href="/create">Create</Nav.Link>
-              </Nav>
-            </Container>
-          </Navbar>
-          <Routes>
-            <Route path='/' element={<Content />} exact />
-            <Route exact path='/create' element={<Header />} />
-            <Route path='/read' element={<Footer />} />
-          </Routes>
+        {/* Navbar component from Bootstrap */}
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+
+            {/* Nav links for navigation */}
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        {/* Setting up routes for different paths */}
+        <Routes>
+          {/* Route for the home page */}
+          <Route path='/' element={<Content />} exact />
+
+          {/* Route for creating content */}
+          <Route exact path='/create' element={<Header />} />
+
+          {/* Route for reading content */}
+          <Route path='/read' element={<Footer />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
+// Export the App component as the default export
 export default App;
--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,7 @@
+// Define a functional component named Content
 function Content(){
     return(
+         // Return a JSX element representing a <div>
         <div>
             <h1>Hello World!</h1>
              <h2>It is {new Date().toLocaleTimeString()}</h2>
